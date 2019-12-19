9f8b61272750f9a7f669949712098acc3faa4186
Jo Hanna Pearce <jo@nrwl.io>
2019-12-19 11:51:15 +0000

    feat : illustration that igx-select does not set control.touched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'igx-select-test';
+
+  things = ['one', 'two', 'three'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  IgxButtonModule,
+  IgxCheckboxModule,
+  IgxComboModule,
+  IgxDatePickerModule,
+  IgxDialogModule,
+  IgxGridModule,
+  IgxIconModule,
+  IgxInputGroupModule,
+  IgxLayoutModule,
+  IgxNavbarModule,
+  IgxNavigationDrawerModule,
+  IgxRippleModule,
+  IgxSelectModule
+} from 'igniteui-angular';
 import { AppComponent } from './app.component';
+import { SelectFormComponent } from './select-form/select-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SelectFormComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+
+    // IGX
+    IgxButtonModule,
+    IgxCheckboxModule,
+    IgxDatePickerModule,
+    IgxDialogModule,
+    IgxGridModule,
+    IgxInputGroupModule,
+    IgxIconModule,
+    IgxLayoutModule,
+    IgxSelectModule,
+    IgxRippleModule,
+    IgxComboModule,
+    IgxNavbarModule,
+    IgxNavigationDrawerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
